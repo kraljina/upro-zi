2258a3da5e46dd95f3123890b88b0e9ec9d56fe9
kraljina <95233470+kraljina@users.noreply.github.com>
2022-02-07 13:23:52 +0100

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void generirajLozinku(unsigned int sjeme, const char *znakoviZaLozinku,
+                      int duljLoz, char *lozinka) {
+    srand(sjeme);
+    int duljNiza = strlen(znakoviZaLozinku);
+    int duljina = 0;
+    while (duljina < duljLoz) {
+        char c = rand() % duljNiza;
+        lozinka[duljina] = znakoviZaLozinku[c];
+        duljina++;
+    }
+    //lozinka[duljina] = '\0'; 
+}
+
+int main(void) {
+    char lozinka[10];
+
+    char niz[100] = "123456789qwertzuiopasdfghjklyxcvbnmQWERTZUIOPASDFGHJKLYXCVBNM#$&()";
+
+    generirajLozinku(
+        500,
+        niz,
+        10, lozinka);
+
+    printf("%s", lozinka);
+
+    return 0;
+}
